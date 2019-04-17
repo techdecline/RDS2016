1ab7dfb84d3bf9d693b217da678e62329d3db30d
Cornelius Schuchardt <cornelius.schuchardt@gmail.com>
2019-04-17 18:07:10 +0200

    Update Distribute-RDSCertificate.ps1

--- Distribute-RDSCertificate.ps1
+++ Distribute-RDSCertificate.ps1
@@ -1,17 +1,27 @@
-$serverList = Get-RDServer
-$serverName = $null
+# Get all Farm Session Hosts
+$sessionHostArr = Get-RDSessionCollection -ConnectionBroker rds-cb1.rds.lab | Get-RDSessionHost
 
-foreach ($serverObj in $serverList) {
-    $serverName = $serverObj.Server
-
-    Invoke-Command -ComputerName $serverName -ScriptBlock {
-        Get-NetFirewallRule -DisplayGroup "File and Printer Sharing" | Enable-NetFirewallRule
+foreach ($sessionHost in $sessionHostArr) {
+    $serverName = $sessionHost.SessionHost
+    Write-Verbose "Current Object is: $($sessionHost.SessionHost)"
+    # Test on SMB Protocol
+    if (-not (Test-NetConnection -ComputerName $serverName -Port 445)) {
+        # Test if target is online
+        if (-not (Test-WSMan $serverName)) {
+            Write-Warning "Connectivity Check failed for: $serverName"
+            continue
+        }
+        else {
+            # Remotely enable SMB Firewall Rules
+            Invoke-Command -ComputerName $serverName -ScriptBlock {
+                Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing"
+            }
+        }
     }
-
-    Copy-Item '\\rds-dc1\c$\wildcard.pfx' -Destination "\\$serverName\c$\wildcard.pfx"
+    Copy-Item C:\cb.pfx -Destination "\\$serverName\c$\cb.pfx"
     Invoke-Command -ComputerName $serverName -ScriptBlock {
-        $password = ConvertTo-SecureString -AsPlainText -Force "Passw0rd"
-        Import-PfxCertificate -Exportable -Password $password -CertStoreLocation Cert:\LocalMachine\My `
-            -FilePath C:\wildcard.pfx
+        $securePassword = ConvertTo-SecureString -AsPlainText -Force "Passw0rd"
+        Import-PfxCertificate -Exportable -CertStoreLocation Cert:\LocalMachine\My `
+            -FilePath C:\cb.pfx -Password $securePassword
     }
 }
